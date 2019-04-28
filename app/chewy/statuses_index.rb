3f5153f50bf421480e0e894689b223fcda0e8083
fono09 <fono@fono.jp>
2019-04-29 08:11:59 +0900

    Elasticsearch有効化

--- app/chewy/statuses_index.rb
+++ app/chewy/statuses_index.rb
@@ -1,35 +1,25 @@
 # frozen_string_literal: true
 
 class StatusesIndex < Chewy::Index
-  settings index: { refresh_interval: '15m' }, analysis: {
-    filter: {
-      english_stop: {
-        type: 'stop',
-        stopwords: '_english_',
-      },
-      english_stemmer: {
-        type: 'stemmer',
-        language: 'english',
-      },
-      english_possessive_stemmer: {
-        type: 'stemmer',
-        language: 'possessive_english',
-      },
-    },
-    analyzer: {
-      content: {
-        tokenizer: 'uax_url_email',
-        filter: %w(
-          english_possessive_stemmer
-          lowercase
-          asciifolding
-          cjk_width
-          english_stop
-          english_stemmer
-        ),
-      },
-    },
-  }
+    settings index: { refresh_interval: '15m' }, analysis: {
+        tokenizer: {
+            kuromoji: {
+                type: 'kuromoji_tokenizer',
+            },
+        },
+        analyzer: {
+            content: {
+                type: 'custom',
+                tokenizer: 'kuromoji',
+                filter: %w(
+                    kuromoji_baseform
+                    kuromoji_stemmer
+                    cjk_width
+                    lowercase
+                ),
+            },
+        },
+    }
 
   define_type ::Status.unscoped.without_reblogs do
     crutch :mentions do |collection|
