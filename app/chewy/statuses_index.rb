8c0206293768717e38973e7c83cc0c08854bf9ab
fono09 <fono@fono.jp>
2020-11-01 01:47:38 +0900

    一旦kuromojiやめる

--- app/chewy/statuses_index.rb
+++ app/chewy/statuses_index.rb
@@ -1,25 +1,35 @@
 # frozen_string_literal: true
 
 class StatusesIndex < Chewy::Index
-    settings index: { refresh_interval: '15m' }, analysis: {
-        tokenizer: {
-            kuromoji: {
-                type: 'kuromoji_tokenizer',
-            },
-        },
-        analyzer: {
-            content: {
-                type: 'custom',
-                tokenizer: 'kuromoji',
-                filter: %w(
-                    kuromoji_baseform
-                    kuromoji_stemmer
-                    cjk_width
-                    lowercase
-                ),
-            },
-        },
-    }
+  settings index: { refresh_interval: '15m' }, analysis: {
+    filter: {
+      english_stop: {
+        type: 'stop',
+        stopwords: '_english_',
+      },
+      english_stemmer: {
+        type: 'stemmer',
+        language: 'english',
+      },
+      english_possessive_stemmer: {
+        type: 'stemmer',
+        language: 'possessive_english',
+      },
+    },
+    analyzer: {
+      content: {
+        tokenizer: 'uax_url_email',
+        filter: %w(
+          english_possessive_stemmer
+          lowercase
+          asciifolding
+          cjk_width
+          english_stop
+          english_stemmer
+        ),
+      },
+    },
+  }
 
   define_type ::Status.unscoped.kept.without_reblogs.includes(:media_attachments, :preloadable_poll) do
     crutch :mentions do |collection|
